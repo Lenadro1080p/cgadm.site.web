f77a9275eead5cbb7fa5621c25364178185f65e2
leandro.jesus0004@gmail.com <Mega123z>
2023-03-01 11:02:54 -0300

    EE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Site';
+  slides = [
+    { img: 'container' },
+  ];
+  slide = [
+    { img: 'container' },
+  ];
+  slid = [
+    { img: 'container' },
+  ];
+  slideConfig = { dots: true,
+    arrows: false,
+    speed: 600,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ], 
+  };
+  addSlide() {
+    this.slides.push({ img: 'http://placehold.it/350x150/' });
+  }
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+  beforeChange(e: any) {
+    console.log('beforeChange');
+  }
+  constructor() {}
+  ngOnInit(): void {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { EventsComponent } from './events/events.component';
+import { PlayerComponent } from './player/player.component';
+import { MinistriesComponent } from './ministries/ministries.component';
+import { CallUsComponent } from './call-us/call-us.component';
+import { OurBlogComponent } from './our-blog/our-blog.component';
+import { EndPageComponent } from './end-page/end-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    BannerComponent,
+    AboutUsComponent,
+    EventsComponent,
+    PlayerComponent,
+    MinistriesComponent,
+    CallUsComponent,
+    OurBlogComponent,
+    EndPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlickCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
